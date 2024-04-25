fa311a285174dae51ae6faf3593c1c3f1a33cae8
HobbaZ <zachobba@gmail.com>
2024-04-26 00:52:34 +1000

    Basic outline of portfolio

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_font_awesome import FontAwesome
+
+app = Flask(__name__)
+font_awesome = FontAwesome(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    return render_template('games.html')
