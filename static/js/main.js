f6974732f2c69507d83361b594b021fc5bf050ad
HobbaZ <zachobba@gmail.com>
2024-05-09 00:03:23 +1000

    Almost completed test game, trying optimising and performance enhancements

--- static/js/main.js
+++ static/js/main.js
@@ -1,284 +1,261 @@
 // main.js
 import * as THREE from "https://cdn.skypack.dev/three@0.133.1";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls";
-
-const clock = new THREE.Clock();
-let delta;
-
-const world = new CANNON.World();
-world.gravity.set(0, -9.82, 0); //Earth's gravity
-world.broadphase = new CANNON.NaiveBroadphase();
-world.solver.iterations = 10;
-world.allowSleep = true;
+//import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls";
 
 document.body.style.overflow = "hidden";
+const pointsUI = document.querySelector("#ui");
+const startScreen = document.querySelector("#startScreen");
+const playBtn = document.querySelector("#playBtn");
+const gameOverScreen = document.querySelector("#gameOverScreen");
+const gameOverMessage = document.querySelector("#gameOverMessage");
+const playAgainBtn = document.querySelector("#playAgainBtn");
 
-const pointsUI = document.querySelector("#points");
 let points = 0;
 let gameOver = true;
-let gameStart = false;
+let gameStart = true;
 let gameTitleName = "Test Game";
+let enemies = [];
+let powerups = [];
+let speed = 0.3;
+let totalObjects = 7;
 
-const gameStartScreen = document.querySelector("#gameTitleScreen");
-const gameTitle = document.querySelector("#gameTitle");
-const playButton = document.querySelector("#playButton");
-const controlsButton = document.querySelector("#controlsButton");
-const gameOverScreen = document.querySelector("#gameOverScreen");
-const playAgain = document.querySelector("#playAgain");
+let camera, scene, renderer, player, playerBox;
 
-function showGameStartScreen() {
-  gameStartScreen.style.display = "block";
-  pointsUI.style.display = "hidden";
-  gameTitle.textContent = gameTitleName;
-  playButton.addEventListener("click", () => {
-    gameStartScreen.style.display = "none";
-    gameStart = true;
-  });
-}
-
-showGameStartScreen();
-
-function showGameOver() {
-  gameOver = true;
-  gameOverScreen.style.display = "block";
-  gameOverScreen.textContent = "GAME OVER n you scored " + points;
-  if (playAgain) {
-    gameStart = true;
-  }
-}
-
-function showControls() {
-  gameScreen.style.display = "none";
-}
-
-function startScreen() {
-  playButton.addEventListener("click", () => {
-    gameScreen.style.display = "none";
-    gameStart = true;
-  });
-
-  controlsButton.addEventListener("click", () => {
-    showControls();
-  });
-}
-
-// generate a random whole number
-const randomRangeNum = (min, max) => {
+// Generate a random whole number
+function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-// move obstacles toward player
-const moveObstacles = (arr, speed, maxX, minX, maxZ, minZ) => {
-  arr.forEach((el) => {
-    el.body.position.z += speed;
-    if (el.body.position.z > camera.position.z) {
-      el.body.position.x = randomRangeNum(maxX, minX);
-      el.body.position.z = randomRangeNum(maxZ, minZ);
-    }
-    el.mesh.position.copy(el.body.position);
-    el.mesh.quaternion.copy(el.body.quaternion);
-  });
-};
-
-var Colors = {
-  red: 0xf25346,
-  white: 0xd8d0d1,
-  brown: 0x59332e,
-  pink: 0xf5986e,
-  brownDark: 0x23190f,
-  blue: 0x68c3c0,
-};
-
-const scene = new THREE.Scene();
-
-scene.fog = new THREE.FogExp2(Colors.pink, 0.1, 50);
-
-const camera = new THREE.PerspectiveCamera(
-  75, //field of view
-  window.innerWidth / window.innerHeight, //aspect ratio
-  0.1, //near plane
-  1000 //far plane
-);
-
-//set camera position
-camera.position.z = 5;
-
-//Renderer creation
-const renderer = new THREE.WebGLRenderer({
-  alpha: true,
-  antialias: true,
-  canvas: document.getElementById("gameCanvas"),
-});
-//Set renderer size to full screen
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Auto resize window
-window.addEventListener("resize", onWindowResize, false);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-//create player
-const player = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshNormalMaterial()
-);
-scene.add(player);
+function init() {
+  console.log("initialising scene");
+  scene = new THREE.Scene();
 
-const playerBody = new CANNON.Body({
-  mass: 1,
-  shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
-  fixedRotation: true,
-});
-world.addBody(playerBody);
+  while (scene.children.length > 0) {
+    scene.remove(scene.children[0]);
+  }
 
-//create ground
-const ground = new THREE.Mesh(
-  new THREE.BoxGeometry(30, 1, 30),
-  new THREE.MeshBasicMaterial({ color: Colors.brown })
-);
-ground.position.y = -1;
-scene.add(ground);
+  scene.fog = new THREE.FogExp2(0o0, 0.1, 50);
+  camera = new THREE.PerspectiveCamera(
+    75, //field of view
+    window.innerWidth / window.innerHeight, //aspect ratio
+    0.1, //near plane
+    1000 //far plane
+  );
 
-const groundBody = new CANNON.Body({
-  shape: new CANNON.Box(new CANNON.Vec3(15, 0.5, 15)),
-});
-world.addBody(groundBody);
+  //set camera position
+  camera.position.set(0, 1, 4);
+
+  createGround();
+
+  createPlayer();
+
+  createLights();
+
+  createPowerups();
+
+  createEnemies();
+
+  //Renderer creation
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+    canvas: document.getElementById("gameCanvas"),
+  });
+  //Set renderer size to full screen
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // Auto resize window
+  window.addEventListener("resize", onWindowResize, false);
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  return;
+}
+
+if (gameStart) {
+  gameOverScreen.style.display = "none";
+  startScreen.style.display = "block";
+  pointsUI.style.display = "none";
+}
+
+// create player
+function createPlayer() {
+  player = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xe56956 })
+  );
+  playerBox = new THREE.Box3().setFromObject(player);
+  scene.add(player);
+  console.log("Added player");
+}
+
+//Add lights
+function createLights() {
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.position.set(10, 20, 0);
+  scene.add(ambientLight, directionalLight);
+  console.log("Added lights");
+}
+
+// Add Ground
+function createGround() {
+  const ground = new THREE.Mesh(
+    new THREE.BoxGeometry(30, 1, 30),
+    new THREE.MeshBasicMaterial({ color: "brown" })
+  );
+  ground.position.y = -1;
+  scene.add(ground);
+  console.log("Added ground");
+}
 
 //create powerups
-const powerups = [];
-for (let i = 0; i < 10; i++) {
-  const powerup = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial({ color: Colors.red })
-  );
-  powerup.name = "powerup" + i + 1;
-  powerup.scale.set(0.2, 0.2, 0.2);
-  const posX = randomRangeNum(8, -8);
-  const posZ = randomRangeNum(5, -5);
-  scene.add(powerup);
-
-  const powerupBody = new CANNON.Body({
-    shape: new CANNON.Sphere(0.2),
-  });
-  powerupBody.position.set(posX, 0, posZ);
-  world.addBody(powerupBody);
-
-  const powerupObject = {
-    mesh: powerup,
-    body: powerupBody,
-  };
-  powerups.push(powerupObject);
+//let powerups = [];
+function createPowerups() {
+  for (let i = 0; i < totalObjects; i++) {
+    const powerup = new THREE.Mesh(
+      new THREE.BoxGeometry(0.5, 0.5, 0.5),
+      new THREE.MeshBasicMaterial({ color: "blue" })
+    );
+    powerup.name = "powerup" + i + 1;
+    const posX = randomNumber(-8, 8);
+    const posZ = randomNumber(-35, -10);
+    powerup.position.set(posX, 0, posZ);
+    scene.add(powerup);
+    powerups.push(powerup);
+  }
+  console.log("Added powerups");
 }
 
-//create enemies
-const enemies = [];
-for (let i = 0; i < 10; i++) {
-  const enemy = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial({ color: Colors.blue })
-  );
-  enemy.name = "enemy" + i + 1;
-  enemy.scale.set(0.5, 0.5, 0.5);
-  const enemyPosX = randomRangeNum(8, -8);
-  const enemyPosZ = randomRangeNum(5, -5);
-  scene.add(enemy);
-
-  const enemyBody = new CANNON.Body({
-    shape: new CANNON.Sphere(0.2),
-  });
-  enemyBody.position.set(enemyPosX, 0, enemyPosZ);
-  world.addBody(enemyBody);
-
-  const enemyObject = {
-    mesh: enemy,
-    body: enemyBody,
-  };
-  enemies.push(enemyObject);
+// create enemies
+function createEnemies() {
+  for (let i = 0; i < totalObjects; i++) {
+    const enemy = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({ color: "red" })
+    );
+    enemy.name = "enemy" + i + 1;
+    const posX = randomNumber(-8, 8);
+    const posZ = randomNumber(-35, -20);
+    enemy.position.set(posX, 0, posZ);
+    scene.add(enemy);
+    enemies.push(enemy);
+  }
+  console.log("Added enemies");
 }
 
-// player collision
-playerBody.addEventListener("collide", (e) => {
-  powerups.forEach((el) => {
-    if (e.body === el.body) {
-      el.body.position.x = randomRangeNum(8, -8);
-      el.body.position.z = randomRangeNum(-5, -10);
-      el.mesh.position.copy(el.body.position);
-      el.mesh.quaternion.copy(el.body.quaternion);
+function detectCollisions() {
+  // Update player's box
+  playerBox.setFromObject(player);
+
+  // Check collisions with enemies
+  for (let i = 0; i < enemies.length; i++) {
+    const enemyBox = new THREE.Box3().setFromObject(enemies[i]);
+    // An object was hit
+    if (enemyBox.intersectsBox(playerBox)) {
+      gameOver = true;
+      console.log("game over");
+      return;
+    }
+  }
+
+  // Check collisions with powerups
+  for (let i = 0; i < powerups.length; i++) {
+    const powerupBox = new THREE.Box3().setFromObject(powerups[i]);
+    // An object was hit
+    if (powerupBox.intersectsBox(playerBox)) {
+      // Remove the powerup from the scene
+      scene.remove(powerups[i]);
+      // Remove it from the array
+      powerups.splice(i, 1);
+      // Update points
       points += 1;
       pointsUI.textContent = points.toString();
+      // Don't return immediately, continue checking for other collisions
     }
-  });
+  }
+}
 
-  //enemy collision
-  enemies.forEach((el) => {
-    if (e.body === el.body) {
-      gameOver = true;
-    }
-  });
-});
-
-//player movement
 window.addEventListener("keydown", (e) => {
   if (e.key === "d" || e.key === "ArrowRight") {
-    player.position.x += 0.05;
+    player.position.x += speed;
   }
-
   if (e.key === "a" || e.key === "ArrowLeft") {
-    player.position.x -= 0.05;
+    player.position.x -= speed;
   }
-
   if (e.key === "r") {
-    player.position.x = 0;
-    player.position.y = 0;
-    player.position.z = 0;
-  }
-
-  if (e.key === " ") {
-    player.position.y = 2;
+    player.position.set(0, 0, 0); // Reset player position
   }
 });
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target.y = 0.5;
-
-controls.update();
+// Move obstacles toward player
+function moveObstacles(arr, power, maxX, minX, maxZ, minZ) {
+  arr.forEach((el) => {
+    el.position.z += power;
+    console.log("enemy speed " + power);
+    if (el.position.z > camera.position.z) {
+      el.position.x = randomNumber(minX, maxX);
+      el.position.z = randomNumber(minZ, maxZ);
+    }
+  });
+}
 
 function animate() {
-  if (!gameOver) {
-    delta = clock.getDelta();
-    world.step(delta);
-    requestAnimationFrame(animate);
-    moveObstacles(powerups, 0.1, 8, -8, -5, -10);
-    moveObstacles(enemies, 0.2, 8, -8, -5, -10);
-  } else {
-    showGameOver();
-    playerBody.velocity.set(playerBody.position.x, 5, 5);
-
+  if (gameOver) {
+    gameOverScreen.style.display = "block";
+    pointsUI.style.display = "none";
+    gameOverMessage.innerHTML =
+      "<div><h1>GAME OVER</h1><br/><h3>You scored " +
+      points.toString() +
+      `${points === 1 ? " point" : " points"}</h3></div>`;
     enemies.forEach((el) => {
-      scene.remove(el.mesh);
-      world.removeBody(el.body);
+      scene.remove(el);
     });
 
     powerups.forEach((el) => {
-      scene.remove(el.mesh);
-      world.removeBody(el.body);
+      scene.remove(el);
     });
 
-    if (playerBody.position.z > camera.position.z) {
+    if (player.position.z > camera.position.z) {
       scene.remove(player);
-      world.removeBody(playerBody);
     }
-  }
 
-  controls.update();
-  playerBody.position.copy(player.position);
-  playerBody.quaternion.copy(player.quaternion);
-  renderer.render(scene, camera);
+    console.log("game ended");
+    return;
+  } else {
+    moveObstacles(powerups, 0.05, -8, 8, -30, -25);
+    moveObstacles(enemies, 0.1, -8, 8, -30, -25);
+
+    detectCollisions();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
 }
 
-animate();
+// Add an event listener for the play button
+playAgainBtn.addEventListener("click", () => {
+  // Hide the play button screen
+  gameOverScreen.style.display = "none";
+  // Clear previous game state
+  resetGameState();
+});
+
+// Add an event listener for the play button
+playBtn.addEventListener("click", () => {
+  // Hide the play button screen
+  startScreen.style.display = "none";
+  // Clear previous game state
+  resetGameState();
+});
+
+// Function to reset game state
+function resetGameState() {
+  points = 0;
+  gameOver = false;
+  enemies = [];
+  powerups = [];
+  init();
+  animate();
+}
